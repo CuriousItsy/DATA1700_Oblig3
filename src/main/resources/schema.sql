dabda5b8d09214e3072674d56105f30400e7c199
Hristo Kyosev <143337714+CuriousItsy@users.noreply.github.com>
2024-04-11 16:15:59 +0200

    Created the right fields in the schema.sql file.

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,6 +1,10 @@
 CREATE TABLE Ticket(
     id INTEGER AUTO_INCREMENT NOT NULL,
-    navn VARCHAR(255) NOT NULL,
-    adresse VARCHAR(255) NOT NULL,
+    film VARCHAR(255) NOT NULL,
+    number INTEGER NOT NULL,
+    firstName VARCHAR(255) NOT NULL,
+    lastName VARCHAR(255) NOT NULL,
+    phone VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
 );
