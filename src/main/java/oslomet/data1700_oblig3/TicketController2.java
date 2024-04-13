48d1fbd12a0a6d6ee89189a91c6c5e976c541c09
Hristo Kyosev <143337714+CuriousItsy@users.noreply.github.com>
2024-04-13 15:29:32 +0200

    Added data validation to the form as well as RegExp.

--- src/main/java/oslomet/data1700_oblig3/TicketController2.java
+++ src/main/java/oslomet/data1700_oblig3/TicketController2.java
@@ -1,11 +1,16 @@
 package oslomet.data1700_oblig3;
 
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -14,13 +19,17 @@ public class TicketController2 {
     @Autowired
     TicketRepository rep;
 
+
+
     @PostMapping("/saveTicket")
-    public void saveTicket(Ticket ticket) {
+    public void saveTicket(Ticket ticket)  {
         rep.saveTicket(ticket);
-    }
+
+            }
 
     @GetMapping("/getTickets")
-    public List<Ticket> getTickets() {
+    public List<Ticket> getTickets()  {
+
         return rep.getTickets();
     }
     @GetMapping("/getHello")
